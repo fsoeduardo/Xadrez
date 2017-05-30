8ecadf6abf1e3e3eeadbb7a02897bbd24d3f1761
fsoeduardo <fsoeduardo@Eduardos-MacBook-Pro.local>
2017-05-30 08:17:37 -0300

    Movimento do peão

--- src/main/Interface.java
+++ src/main/Interface.java
@@ -11,26 +11,80 @@ public class Interface extends JPanel implements MouseListener, MouseMotionListe
 	 */
 	private static final long serialVersionUID = 1L;
 	private static int tamanhoCasa = 64;
+	static int mouseX, mouseY, novoMouseX, novoMouseY;
 
 	public void paintComponent(Graphics g){
 		for(int i=0; i<64; i+=2){
 			g.setColor(new Color(255, 255, 255));
 			g.fillRect((i%8+(i/8)%2)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa);
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			g.setColor(new Color(0, 0, 0));
+			g.setColor(new Color(100, 100, 100));
 			g.fillRect(((i+1)%8-((i+1)/8)%2)*tamanhoCasa, ((i+1)/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa);
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		}
+		for(int i=0; i<64; i++){
+			switch (Main.TABULEIRO[i/8][i%8]) {
+	            case "P": 
+	            	Image peaoBranco;
+	            	peaoBranco = new ImageIcon("src/img/white_pawn.png").getImage();
+	            	g.drawImage(peaoBranco, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "p": 
+	            	Image peaoPreto;
+	            	peaoPreto = new ImageIcon("src/img/black_pawn.png").getImage();
+	            	g.drawImage(peaoPreto, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "R":
+	            	Image torreBranca;
+	            	torreBranca = new ImageIcon("src/img/white_rook.png").getImage();
+	            	g.drawImage(torreBranca, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "r":
+	            	Image torrePreta;
+	            	torrePreta = new ImageIcon("src/img/black_rook.png").getImage();
+	            	g.drawImage(torrePreta, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "N": 
+	            	Image cavaloBranco;
+	            	cavaloBranco = new ImageIcon("src/img/white_knight.png").getImage();
+	            	g.drawImage(cavaloBranco, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "n": 
+	            	Image cavaloPreto;
+	            	cavaloPreto = new ImageIcon("src/img/black_knight.png").getImage();
+	            	g.drawImage(cavaloPreto, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "B": 
+	            	Image bispoBranco;
+	            	bispoBranco = new ImageIcon("src/img/white_bishop.png").getImage();
+	            	g.drawImage(bispoBranco, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "b": 
+	            	Image bispoPreto;
+	            	bispoPreto = new ImageIcon("src/img/black_bishop.png").getImage();
+	            	g.drawImage(bispoPreto, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "Q": 
+	            	Image rainhaBranca;
+	            	rainhaBranca = new ImageIcon("src/img/white_queen.png").getImage();
+	            	g.drawImage(rainhaBranca, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "q": 
+	            	Image rainhaPreta;
+	            	rainhaPreta = new ImageIcon("src/img/black_queen.png").getImage();
+	            	g.drawImage(rainhaPreta, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "K": 
+	            	Image reiBranco;
+	            	reiBranco = new ImageIcon("src/img/white_king.png").getImage();
+	            	g.drawImage(reiBranco, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
+	            case "k": 
+	            	Image reiPreto;
+	            	reiPreto = new ImageIcon("src/img/black_king.png").getImage();
+	            	g.drawImage(reiPreto, (i%8)*tamanhoCasa, (i/8)*tamanhoCasa, tamanhoCasa, tamanhoCasa, this);
+	                break;
 			}
 		}
+		
 		this.addMouseListener(this);
 		this.addMouseMotionListener(this);
 	}
@@ -56,13 +110,36 @@ public class Interface extends JPanel implements MouseListener, MouseMotionListe
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getX()<8*tamanhoCasa &&e.getY()<8*tamanhoCasa) {
+            //if inside the board
+            mouseX=e.getX();
+            mouseY=e.getY();
+            repaint();
+        }
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getX()<8*tamanhoCasa &&e.getY()<8*tamanhoCasa) {
+            //Confere se está dentro do tabuleiro
+            novoMouseX=e.getX();
+            novoMouseY=e.getY();
+            String arrasto;
+            //if (novoMouseY/tamanhoCasa==0 && mouseY/tamanhoCasa==1 && "P".equals(Main.TABULEIRO[mouseY/tamanhoCasa][mouseX/tamanhoCasa])) {
+                //Promove peão
+            	//arrasto=""+mouseX/tamanhoCasa+novoMouseX/tamanhoCasa+Main.TABULEIRO[novoMouseY/tamanhoCasa][novoMouseX/tamanhoCasa]+"QP";
+            //} else {
+                //regular move
+            	arrasto=""+mouseY/tamanhoCasa+mouseX/tamanhoCasa+novoMouseY/tamanhoCasa+novoMouseX/tamanhoCasa+Main.TABULEIRO[novoMouseY/tamanhoCasa][novoMouseX/tamanhoCasa];
+            //}
+            //String posibilidades=Main.movimentosValidos();
+            //if (posibilidades.replaceAll(arrasto, "").length()<posibilidades.length()) {
+                //Movimento válido
+                Main.movimenta(arrasto);
+            //}
+            repaint();
+        }
 	}
 
 	@Override
--- src/main/Main.java
+++ src/main/Main.java
@@ -3,6 +3,30 @@ package main;
 import javax.swing.*;
 
 public class Main {
+	/*
+		Usando notação algébrica em inglês para definir a posição inicial do tabuleiro
+		Letras minúsculas indicam peças pretas e maiúsculas, brancas
+		K = King
+		Q = Queen
+		N = Knight
+		B = Bishop
+		R = Rook
+		P = Pawn
+	*/
+	static String TABULEIRO[][]={
+			{"r", "n", "b", "q", "k", "b", "n", "r"},
+			{"p", "p", "p", "p", "p", "p", "p", "p"},
+			{" ", " ", " ", " ", " ", " ", " ", " "},
+			{" ", " ", " ", " ", " ", " ", " ", " "},
+			{" ", " ", " ", " ", " ", " ", " ", " "},
+			{" ", " ", " ", " ", " ", " ", " ", " "},
+			{"P", "P", "P", "P", "P", "P", "P", "P"},
+			{"R", "N", "B", "Q", "K", "B", "N", "R"},
+	};
+	
+	//Usado para conferir se o rei está em xeque
+	static int posicaoReiBranco, posicaoReiPreto;
+	
 	public static void main(String[] args) {
 		Interface ui = new Interface();
 		
@@ -14,4 +38,65 @@ public class Main {
 		frame.setResizable(false);
 		frame.setVisible(true);
 	}
+	
+	public static String movimentosValidos() {
+        String list="";
+        for (int i=0; i<64; i++) {
+            switch (TABULEIRO[i/8][i%8]) {
+                case "P": list+=validoP(i);
+                    break;
+                case "R": list+=validoR(i);
+                    break;
+                case "N": list+=validoN(i);
+                    break;
+                case "B": list+=validoB(i);
+                    break;
+                case "Q": list+=validoQ(i);
+                    break;
+                case "K": list+=validoK(i);
+                    break;
+            }
+        }
+        return list;
+    }
+	
+	public static String validoP(int i) {
+        String list="";
+        return list;
+    }
+	public static String validoR(int i) {
+        String list="";
+        return list;
+    }
+	public static String validoN(int i) {
+        String list="";
+        return list;
+    }
+	public static String validoB(int i) {
+        String list="";
+        return list;
+    }
+	public static String validoQ(int i) {
+        String list="";
+        return list;
+    }
+	public static String validoK(int i) {
+        String list="";
+        return list;
+    }
+	
+	public static void movimenta(String m) {
+        if (m.charAt(4)!='P') {
+            TABULEIRO[Character.getNumericValue(m.charAt(2))][Character.getNumericValue(m.charAt(3))]=TABULEIRO[Character.getNumericValue(m.charAt(0))][Character.getNumericValue(m.charAt(1))];
+            TABULEIRO[Character.getNumericValue(m.charAt(0))][Character.getNumericValue(m.charAt(1))]=" ";
+            if ("K".equals(TABULEIRO[Character.getNumericValue(m.charAt(2))][Character.getNumericValue(m.charAt(3))])) {
+                posicaoReiBranco=8*Character.getNumericValue(m.charAt(2))+Character.getNumericValue(m.charAt(3));
+            }
+        } /*else {
+            //Promove peão
+        	TABULEIRO[1][Character.getNumericValue(m.charAt(0))]=" ";
+        	TABULEIRO[0][Character.getNumericValue(m.charAt(1))]=String.valueOf(m.charAt(3));
+        }*/
+    }
+	
 }
